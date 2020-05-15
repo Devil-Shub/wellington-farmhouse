efe452407303981afdf6a57ec01e4013dd3d979b
kdinfo13 <kd.saini@mail.com>
2020-05-16 00:19:45 +0530

    add regsiter page toaster

--- resources/js/_helpers/handle-response.js
+++ resources/js/_helpers/handle-response.js
@@ -4,11 +4,11 @@ export function handleResponse(response) {
   return response.text().then(text => {
     const data = text && JSON.parse(text);
     if (!response.ok) {
-      if ([401, 403].indexOf(response.status) !== -1) {
+      if ([401, 403, 422].indexOf(response.status) !== -1) {
         // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
         authenticationService.logout();
         // location.reload(true);
-      } else if ([422].indexOf(response.status) !== -1) {
+      } else if ([422, 500].indexOf(response.status) !== -1) {
         for (let [key, value] of Object.entries(data.data)) {
           var x = document.getElementById("snackbar");
           x.innerHTML = `${value}`;
